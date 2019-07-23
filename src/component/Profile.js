a2cf39b651faff827f9e8a88b21043369006069c
ram.sapkota <ram.sapkota@sourcecode.com.np>
2019-07-23 10:56:44 +0545

    Logoup in profile section

--- src/component/Profile.js
+++ src/component/Profile.js
@@ -15,6 +15,7 @@ import LocationOn from "@material-ui/icons/LocationOn";
 import LinkIcon from "@material-ui/icons/Link";
 import CalendarToday from "@material-ui/icons/CalendarToday";
 import EditIcon from "@material-ui/icons/Edit";
+import KeyboardReturn from "@material-ui/icons/KeyboardReturn";
 
 import { connect } from "react-redux";
 import { logoutUser, uploadImage } from "../redux/actions/userActions";
@@ -79,6 +80,9 @@ class Profile extends Component {
     const fileInput = document.getElementById("image-input");
     fileInput.click();
   };
+  handleLogout = () => {
+    this.props.logoutUser();
+  };
 
   render() {
     const {
@@ -140,6 +144,11 @@ class Profile extends Component {
               <CalendarToday color="primary" />{" "}
               <span>Joined {dayjs(createdAt).format("MMM YYYY")}</span>
             </div>
+            <ToolTip title="Logout" placement="top">
+              <IconButton onClick={this.handleLogout}>
+                <KeyboardReturn color="primary" />
+              </IconButton>
+            </ToolTip>
           </div>
         </Paper>
       ) : (
